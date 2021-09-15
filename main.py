cbf89a7d764147b8b68a3ad64d2bb36949cbb989
maristie <me@maristie.com>
2021-09-15 19:47:36 +0900

    Amortize requests over time evenly

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import asyncio
+import itertools
 import random
-from collections import defaultdict
 
 import task
 from requester import Requester
 from targets import Target
 
-DELAY = 60
+DELAY = 15
 
 
 async def main(req: Requester):
-    while True:
-        tgt_by_checker = defaultdict(list)
-        for tgt in Target.rand_iter():
-            tgt_by_checker[tgt.value.checker].append(tgt)
-        await asyncio.gather(*(task.create_intervaled_tasks(req, *task_list) for task_list in tgt_by_checker.values()))
-        await asyncio.sleep(DELAY + random.uniform(-5, 20))
+    for tgt in itertools.cycle(Target):
+        await task.create_task(req, tgt)
+        await asyncio.sleep(DELAY + random.uniform(-1.25, 5))
 
 asyncio.run(main(Requester()))
