bdd25bb865e82f698b903f2279cf3a24ab3218fb
maristie <me@maristie.com>
2021-09-21 20:59:32 +0900

    Make in_stock decision stricter

--- parsing/msft.py
+++ parsing/msft.py
@@ -1,3 +1,3 @@
 # msft alternative probing URL: https://www.xbox.com/ja-jp/configure/942j774tp9jn
 def in_stock(text):
-    return text.count('在庫なし') < 2
+    return text.count('予約注文') != 1 or text.count('在庫なし') != 2
