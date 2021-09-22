ef5d249b9dff893b58925f9b46905072fa43018f
maristie <me@maristie.com>
2021-09-22 11:35:56 +0900

    Handle validation required by biccamera

--- parsing/biccamera.py
+++ parsing/biccamera.py
@@ -1,2 +1,4 @@
 def in_stock(text):
-    return 'We are crowded very much' not in text and 'This product became out of stock' not in text
+    return ('We are crowded very much' not in text
+            and 'For security protection, I would like cooperation' not in text
+            and 'This product became out of stock' not in text)
