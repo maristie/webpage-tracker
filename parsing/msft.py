039ca058168d7ed39ca10617d1de896d5bd088c8
maristie <me@maristie.com>
2021-09-30 21:52:28 +0900

    Fix msft

--- parsing/msft.py
+++ parsing/msft.py
@@ -1,3 +1,2 @@
-# msft alternative probing URL: https://www.xbox.com/ja-jp/configure/942j774tp9jn
 def in_stock(text):
-    return text.count('予約注文') != 1 or text.count('在庫なし') != 2
+    return '会計' in text
